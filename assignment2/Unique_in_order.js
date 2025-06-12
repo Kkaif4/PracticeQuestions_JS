35594e0b86233154056cab191e3205ad4bc1a08e
Kkiaf4 <kkaifshaikh.27@gmail.com>
2025-06-12 16:00:03 +0530

    solved folding array and unique in order question from assignment 2

---
+++ assignment2/Unique_in_order.js
@@ -0,0 +1,17 @@
+let string = 'aaabbbccdaaabb';
+
+let str = string.split('');
+console.log(str);
+let s = 0;
+let k = 1;
+for (let i = 0; i < str.length; i++) {
+  if (str[s] === str[k]) {
+    str.splice(k, 1);
+    i--;
+  } else {
+    s++;
+    k++;
+  }
+}
+
+console.log(str);
